65761a4eded909611f71ac7fc44b3c4e27842954
Dale Larroder <dlarroder@gmail.com>
2022-06-16 18:51:54 +0800

    fix paths

--- components/comments/index.tsx
+++ components/comments/index.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 
 const GiscusComponent = dynamic(
   () => {
-    return import('./Giscus')
+    return import('@/components/Comments/Giscus')
   },
   { ssr: false }
 )
