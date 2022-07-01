b06327b4de7fd187edd893ecc005ad2db5227a1e
Dale Larroder <dlarroder@gmail.com>
2022-07-01 21:39:34 +0800

    add more ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://cdn.logrocket.io https://cdn.lr-ingest.io https://cdn.lr-in.com https://cdn.lr-in-prod.com;
+  child-src 'self' blob:; worker-src 'self' blob:; 
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
