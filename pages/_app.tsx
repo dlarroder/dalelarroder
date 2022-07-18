62503b625131b218199072e21f2d06ce12b0be95
dlarroder <dlarroder@gmail.com>
2022-07-18 18:07:14 +0800

    fix import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,16 @@ import '@/css/tailwind.css'
 import '@fontsource/dm-sans'
 import '@fontsource/merriweather'
 
+import ProgressBar from '@/components/ProgressBar'
 import { ScrollObserver } from '@/components/ScrollObserver'
 import ScrollProgressBar from '@/components/ScrollProgressBar'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
-import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import useLogRocket from '../lib/hooks/useLogRocket'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const ProgressBar = dynamic(() => import('@/components/ProgressBar/ProgressBar'), { ssr: false })
-
   useLogRocket()
 
   return (
