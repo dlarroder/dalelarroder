897e65c0d6dd910a0183ed43cf76928e251c5f65
dlarroder <dlarroder@gmail.com>
2022-08-07 20:30:26 +0800

    fix: mobile nav scroll

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -23,7 +23,7 @@ const MobileNav = () => {
         type="button"
         className="w-8 h-8 py-1 ml-1 mr-1 rounded"
         aria-label="Toggle Menu"
-        onClick={(e) => setNavShow(!navShow)}
+        onClick={() => setNavShow(!navShow)}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
