f67675572dff78c84e35115301e826af65fe46ba
Dale Larroder <dlarroder@gmail.com>
2022-08-16 22:27:24 +0800

    move more out

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,32 +18,19 @@ const siteMetadata = {
   spotify: 'https://open.spotify.com/user/12162121994?si=e685b3546f414967',
   steam: 'https://steamcommunity.com/id/dlarroder/',
   locale: 'en-US',
-  newsletter: {
-    provider: 'buttondown',
-  },
   comment: {
     provider: 'giscus',
     giscusConfig: {
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO || '',
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID || '',
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY || '',
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID || '',
       mapping: 'pathname',
       reactions: '1',
       metadata: '0',
       theme: 'light',
       darkTheme: 'transparent_dark',
       themeURL: '',
-    },
-    utterancesConfig: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '',
-      label: '',
-      theme: '',
-      darkTheme: '',
-    },
-    disqusConfig: {
-      shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
     },
   },
 }
