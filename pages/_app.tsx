6beba2c8c39354607f3c4ca991719a1d0405851c
Frank Omondi <65462725+Dofften@users.noreply.github.com>
2023-04-23 13:57:06 +0300

    Add google analytics _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7LBW8EKZH2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7LBW8EKZH2');
+        </script>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       <AnimatePresence exitBeforeEnter initial={false}>
