a6d395cd745d37ce7ba5185351523c84a1480ace
Frank Omondi <65462725+Dofften@users.noreply.github.com>
2023-04-23 14:20:06 +0300

    revert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,21 +11,11 @@ import { AnimatePresence } from 'framer-motion'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
-        <!-- Google tag (gtag.js) -->
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7LBW8EKZH2"/>
-        <Script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-7LBW8EKZH2');
-        </Script>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       <AnimatePresence exitBeforeEnter initial={false}>
