54e61317de9f3a4bb8e4dd6c702677fd54a7fa4a
Dale Larroder <dlarroder@gmail.com>
2024-08-18 21:45:40 +0800

    feat: contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,16 @@
+import Contact from '@/components/Contact/Contact';
 import Hero from '@/components/Hero';
 import Intro from '@/components/Intro';
 import { ScrollProvider } from '@/components/Providers/ScrollProvider';
-import RecentPosts from '@/components/RecentPosts';
-import SectionContainer from '@/components/SectionContainer';
-import TopTracks from '@/components/Spotify/TopTracks';
-import Works from '@/components/Work/Works';
-import { allCoreContent, sortedBlogPost } from '@/lib/utils/contentlayer';
-import { allBlogs } from 'contentlayer/generated';
-import { Suspense } from 'react';
+import Works from '../components/Work/Works';
 
 export default function Page() {
-  const sortedPosts = sortedBlogPost(allBlogs);
-  const posts = allCoreContent(sortedPosts);
-
   return (
     <ScrollProvider>
       <Hero />
       <Intro />
       <Works />
-      <SectionContainer>
-        <RecentPosts posts={posts} />
-        <Suspense fallback="loading..">
-          <TopTracks />
-        </Suspense>
-      </SectionContainer>
+      <Contact />
     </ScrollProvider>
   );
 }
