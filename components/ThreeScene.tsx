08142854e998e0da01cd99d3938eddacab64362a
alcuin <alcuin@alcuindeMac-mini.local>
2024-08-19 02:57:04 +0800

    whaledemmo

--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -3,7 +3,6 @@
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import {
-  MeshTransmissionMaterial,
   useGLTF,
   AccumulativeShadows,
   RandomizedLight,
@@ -11,13 +10,11 @@ import {
   OrbitControls,
   Center,
 } from '@react-three/drei';
-import { useControls } from 'leva';
-
 export default function ThreeScene() {
   return (
     <Canvas
       shadows
-      camera={{ position: [55, 50, 55], fov: 26 }}
+      camera={{ position: [55, 20, 55], fov: 26 }}
       style={{ width: '100vw', height: '100vh' }}
     >
       <ambientLight intensity={Math.PI} />
@@ -56,128 +53,14 @@ export default function ThreeScene() {
 }
 
 function GelatinousCube() {
-  const config = useControls({
-    meshPhysicalMaterial: false,
-    transmissionSampler: false,
-    backside: false,
-    samples: { value: 10, min: 1, max: 32, step: 1 },
-    resolution: { value: 2048, min: 256, max: 2048, step: 256 },
-    transmission: { value: 1, min: 0, max: 1 },
-    roughness: { value: 0.0, min: 0, max: 1, step: 0.01 },
-    thickness: { value: 3.5, min: 0, max: 10, step: 0.01 },
-    ior: { value: 1.01, min: 1, max: 5, step: 0.01 },
-    chromaticAberration: { value: 0.04, min: 0, max: 1 },
-    anisotropy: { value: 0.1, min: 0, max: 1, step: 0.01 },
-    distortion: { value: 0.57, min: 0, max: 1, step: 0.01 },
-    distortionScale: { value: 0.5, min: 0.01, max: 1, step: 0.01 },
-    temporalDistortion: { value: 0.5, min: 0, max: 1, step: 0.01 },
-    clearcoat: { value: 1, min: 0, max: 1 },
-    attenuationDistance: { value: 0.5, min: 0, max: 10, step: 0.01 },
-    attenuationColor: '#ffffff',
-    color: '#99ecff',
-    bg: '#839681',
-  });
-
-  const { nodes, materials } = useGLTF('./frozenwhaleL.glb');
+  const { nodes, materials } = useGLTF('./bluewhale.glb');
   console.log(nodes); // 检查 nodes 对象中的所有几何体
 
   return (
     <group dispose={null}>
-      <mesh geometry={(nodes.cube1 as THREE.Mesh).geometry} position={[-0.56, -1.38, -0.11]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={(nodes.cube2 as THREE.Mesh).geometry}
-        material={materials.cube_mat}
-        material-side={THREE.FrontSide}
-        position={[-0.56, -1.38, -0.11]}
-      />
-      <mesh geometry={(nodes.cube1001 as THREE.Mesh).geometry} position={[-0.56, -1.34, -0.11]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={(nodes.cube2001 as THREE.Mesh).geometry}
-        material={materials.cube_mat}
-        material-side={THREE.FrontSide}
-        position={[-0.56, -1.34, -0.11]}
-      />
-      <mesh geometry={(nodes.cube1002 as THREE.Mesh).geometry} position={[-0.56, -1.38, -0.11]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={(nodes.cube2002 as THREE.Mesh).geometry}
-        material={materials.cube_mat}
-        material-side={THREE.FrontSide}
-        position={[-0.56, -1.38, -0.11]}
-      />
-      <mesh geometry={(nodes.cube1003 as THREE.Mesh).geometry} position={[-4.8, -1.38, -4.6]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={(nodes.cube2003 as THREE.Mesh).geometry}
-        material={materials.cube_mat}
-        material-side={THREE.FrontSide}
-        position={[-4.8, -1.38, -4.6]}
-      />
-      <mesh geometry={(nodes.球体 as THREE.Mesh).geometry} position={[-11.1, 0.38, -9.45]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        geometry={(nodes.bubbles as THREE.Mesh).geometry}
-        material={materials.cube_bubbles_mat}
-        position={[-0.56, -2.28, -0.11]}
-      />
-      <group position={[-1.0, -2, -1.5]}>
-        <mesh geometry={(nodes.arrows as THREE.Mesh).geometry} material={materials.weapons_mat} />
-      </group>
       <mesh
         geometry={(nodes.bluewhale_1 as THREE.Mesh).geometry}
         material={materials.bluewhale}
-        position={[0.0, -1.5, -0.5]}
-        rotation={[0, 1.2 * Math.PI, 0]}
-      />
-      <mesh
-        geometry={(nodes.efish as THREE.Mesh).geometry}
-        material={materials.efish}
-        position={[-0.4, -2.18, 0.1]}
-      />
-      <mesh
-        geometry={(nodes.shark as THREE.Mesh).geometry}
-        material={materials.shark}
-        position={[-0, -1.5, 0.5]}
-        rotation={[0, 2.5 * Math.PI, 0]}
-      />
-      <mesh
-        geometry={(nodes.turtle as THREE.Mesh).geometry}
-        material={materials.turtle}
         position={[-0.5, -1.68, 0.4]}
         rotation={[0, 2.1 * Math.PI, 0]}
       />
