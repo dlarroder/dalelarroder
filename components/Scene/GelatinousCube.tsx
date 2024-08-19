3ed81c9d06d4c73945b54bf1709acf296172b60e
alcuin <alcuin@alcuindeMac-mini.local>
2024-08-19 23:23:33 +0800

    siglecubetest

--- components/Scene/GelatinousCube.tsx
+++ components/Scene/GelatinousCube.tsx
@@ -1,8 +1,6 @@
 import * as THREE from 'three';
 import { MeshTransmissionMaterial, useGLTF } from '@react-three/drei';
 import { useControls } from 'leva';
-import { useRef } from 'react';
-import { useThree } from '@react-three/fiber';
 
 export default function GelatinousCube() {
   const config = useControls({
@@ -28,19 +26,10 @@ export default function GelatinousCube() {
   });
 
   const { nodes, materials } = useGLTF('./frozenwhaleL.glb');
-  const cubeRef = useRef<THREE.Mesh>(null); // 使用 useRef 获取 mesh 的引用
-  const { viewport } = useThree(); // 获取视口信息
 
   return (
-    <group
-      dispose={null}
-      scale={[viewport.width / 3.75, viewport.width / 3.75, viewport.width / 3.75]}
-    >
-      <mesh
-        ref={cubeRef}
-        geometry={(nodes.cube1 as THREE.Mesh).geometry}
-        position={[-0.56, -1.38, -0.11]}
-      >
+    <group dispose={null}>
+      <mesh geometry={(nodes.cube1 as THREE.Mesh).geometry} position={[-0.56, -1.38, -0.11]}>
         {config.meshPhysicalMaterial ? (
           <meshPhysicalMaterial {...config} />
         ) : (
@@ -54,107 +43,6 @@ export default function GelatinousCube() {
         material={materials.cube_mat}
         material-side={THREE.FrontSide}
         position={[-0.56, -1.38, -0.11]}
-      />
-      <mesh
-        ref={cubeRef}
-        geometry={(nodes.cube1001 as THREE.Mesh).geometry}
-        position={[-0.56, -1.34, -0.11]}
-      >
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={(nodes.cube2001 as THREE.Mesh).geometry}
-        material={materials.cube_mat}
-        material-side={THREE.FrontSide}
-        position={[-0.56, -1.34, -0.11]}
-      />
-      <mesh
-        ref={cubeRef}
-        geometry={(nodes.cube1002 as THREE.Mesh).geometry}
-        position={[-0.56, -1.38, -0.11]}
-      >
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={(nodes.cube2002 as THREE.Mesh).geometry}
-        material={materials.cube_mat}
-        material-side={THREE.FrontSide}
-        position={[-0.56, -1.38, -0.11]}
-      />
-      <mesh
-        ref={cubeRef}
-        geometry={(nodes.cube1003 as THREE.Mesh).geometry}
-        position={[-4.8, -1.38, -4.6]}
-      >
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={(nodes.cube2003 as THREE.Mesh).geometry}
-        material={materials.cube_mat}
-        material-side={THREE.FrontSide}
-        position={[-4.8, -1.38, -4.6]}
-      />
-      <mesh
-        ref={cubeRef}
-        geometry={(nodes.球体 as THREE.Mesh).geometry}
-        position={[-11.1, 0.38, -9.45]}
-      >
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        geometry={(nodes.bubbles as THREE.Mesh).geometry}
-        material={materials.cube_bubbles_mat}
-        position={[-0.56, -2.28, -0.11]}
-      />
-      <group position={[-1.0, -2, -1.5]}>
-        <mesh geometry={(nodes.arrows as THREE.Mesh).geometry} material={materials.weapons_mat} />
-      </group>
-      <mesh
-        ref={cubeRef}
-        geometry={(nodes.bluewhale_1 as THREE.Mesh).geometry}
-        material={materials.bluewhale}
-        position={[0.0, -1.5, -0.5]}
-        rotation={[0, 1.2 * Math.PI, 0]}
-      />
-      <mesh
-        geometry={(nodes.efish as THREE.Mesh).geometry}
-        material={materials.efish}
-        position={[-0.4, -2.18, 0.1]}
-      />
-      <mesh
-        geometry={(nodes.shark as THREE.Mesh).geometry}
-        material={materials.shark}
-        position={[-0, -1.5, 0.5]}
-        rotation={[0, 2.5 * Math.PI, 0]}
-      />
-      <mesh
-        ref={cubeRef}
-        geometry={(nodes.turtle as THREE.Mesh).geometry}
-        material={materials.turtle}
-        position={[-0.5, -1.68, 0.4]}
-        rotation={[0, 2.1 * Math.PI, 0]}
       />
     </group>
   );
