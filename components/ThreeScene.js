4a5dd9649e42abb73882dab57daac460bed2868a
alcuin <alcuin@alcuindeMac-mini.local>
2024-08-21 21:46:23 +0800

    sigle cube

--- components/ThreeScene.js
+++ components/ThreeScene.js
@@ -59,96 +59,13 @@ function GelatinousCube() {
         material-side={THREE.FrontSide}
         position={[-0.56, -1.38, -0.11]}
       />
-      <mesh geometry={nodes.cube1_1.geometry} position={[-0.56, -1.34, -0.11]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={nodes.cube2_1.geometry}
-        material={materials.cube_mat1}
-        material-side={THREE.FrontSide}
-        position={[-0.56, -1.34, -0.11]}
-      />
-      <mesh geometry={nodes.cube1_2.geometry} position={[-0.56, -1.38, -0.11]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={nodes.cube2_2.geometry}
-        material={materials.cube_mat2}
-        material-side={THREE.FrontSide}
-        position={[-0.56, -1.38, -0.11]}
-      />
-      <mesh geometry={nodes.cube1_3.geometry} position={[-4.8, -1.38, -4.6]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        castShadow
-        renderOrder={-100}
-        geometry={nodes.cube2_3.geometry}
-        material={materials.cube_mat3}
-        material-side={THREE.FrontSide}
-        position={[-4.8, -1.38, -4.6]}
-      />
-      <mesh geometry={nodes.球体.geometry} position={[-11.1, 0.38, -9.45]}>
-        {config.meshPhysicalMaterial ? (
-          <meshPhysicalMaterial {...config} />
-        ) : (
-          <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />
-        )}
-      </mesh>
-      <mesh
-        geometry={nodes.bubbles.geometry}
-        material={materials.cube_bubbles_mat}
-        position={[-0.56, -2.28, -0.11]}
-      />
-      <group position={[-1.0, -2, -1.5]}>
-        <mesh geometry={nodes.arrows.geometry} material={materials.weapons_mat} />
-      </group>
-      <mesh
-        geometry={nodes.bluewhale_1.geometry}
-        material={materials.bluewhale}
-        position={[0.0, -1.5, -0.5]}
-        rotation={[0, 1.2 * Math.PI, 0]}
-      />
-      <mesh
-        geometry={nodes.efish.geometry}
-        material={materials.efish}
-        position={[-0.4, -2.18, 0.1]}
-      />
-      <mesh
-        geometry={nodes.shark.geometry}
-        material={materials.shark}
-        position={[-0, -1.5, 0.5]}
-        rotation={[0, 2.5 * Math.PI, 0]}
-      />
-      <mesh
-        geometry={nodes.turtle.geometry}
-        material={materials.turtle}
-        position={[-0.5, -1.68, 0.4]}
-        rotation={[0, 2.1 * Math.PI, 0]}
-      />
     </group>
   );
 }
 
 function ThreeScene() {
   return (
-    <Canvas shadows camera={{ position: [55, 20, 55], fov: 26 }}>
+    <Canvas shadows camera={{ position: [55, 20, 55], fov: 26 }} style={{ height: '80vh' }}>
       <ambientLight intensity={Math.PI} />
       <group position={[0, -2.5, 0]}>
         <Center top>
