0eb46e0a37098c59f6db5d71fd0ebbf71391a01c
alcuin <alcuin@alcuindeMac-mini.local>
2024-08-22 22:39:29 +0800

    new solution

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+'use client';
 import Hero from '@/components/Hero';
 import Intro from '@/components/Intro';
-import dynamic from 'next/dynamic';
 import { ScrollProvider } from '@/components/Providers/ScrollProvider';
 import RecentPosts from '@/components/RecentPosts';
 import SectionContainer from '@/components/SectionContainer';
@@ -8,8 +8,8 @@ import TopTracks from '@/components/Spotify/TopTracks';
 import { allCoreContent, sortedBlogPost } from '@/lib/utils/contentlayer';
 import { allBlogs } from 'contentlayer/generated';
 import { Suspense } from 'react';
-
-const ThreeScene = dynamic(() => import('@/components/ThreeScene'), { ssr: false });
+import { Canvas } from '@react-three/fiber';
+import { Model } from '@/components/FrozenwhaleL';
 
 export default function Page() {
   const sortedPosts = sortedBlogPost(allBlogs);
@@ -19,7 +19,12 @@ export default function Page() {
     <ScrollProvider>
       <Hero />
       <Intro />
-      <ThreeScene />
+      {/* 加入3D模型 */}
+      <div style={{ height: '80vh' }}>
+        <Canvas>
+          <Model />
+        </Canvas>
+      </div>
       <SectionContainer>
         <RecentPosts posts={posts} />
         <Suspense fallback="loading..">
