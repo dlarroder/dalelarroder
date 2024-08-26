f639068064f3bdc197b1643ab522159d83f81aa9
Dale Larroder <dlarroder@gmail.com>
2024-08-26 09:07:31 +0800

    theme

--- components/GithubContributions/Calendar.tsx
+++ components/GithubContributions/Calendar.tsx
@@ -1,20 +1,30 @@
 'use client';
 
-import ActivityCalendar, { Activity } from 'react-activity-calendar';
+import { useTheme } from 'next-themes';
+import ActivityCalendar, { Activity, ThemeInput } from 'react-activity-calendar';
 import ActivityTooltip from './ActivityTooltip';
+
+export const DEFAULT_THEME: ThemeInput = {
+  light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
+  dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353'],
+};
 
 interface Props {
   data: Activity[];
 }
 
 export default function Calendar({ data }: Props) {
+  const { theme } = useTheme();
+
   return (
     <div className="flex flex-col space-y-4">
       <p className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
         Github Stats
       </p>
       <ActivityCalendar
+        colorScheme={theme as any}
         data={data}
+        theme={DEFAULT_THEME}
         showWeekdayLabels
         labels={{
           totalCount: '{{count}} contributions within the last year',
