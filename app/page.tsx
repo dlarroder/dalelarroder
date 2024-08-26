a1760268865e0a6b06b611d7bf0b07df7ba6d005
Dale Larroder <dlarroder@gmail.com>
2024-08-27 02:35:34 +0800

    small fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Contact from '@/components/Contact/Contact';
 import Hero from '@/components/Hero';
 import Intro from '@/components/Intro';
 import { ScrollProvider } from '@/components/Providers/ScrollProvider';
-import Works from '../components/Work/Works';
+import Works from '@/components/Work/Works';
 
 export default function Page() {
   return (
