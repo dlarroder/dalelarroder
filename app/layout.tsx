c3c80ae19a055e6e9eea2d4431d4c43216fa7090
Dale Larroder <dlarroder@gmail.com>
2024-12-15 11:06:58 +0800

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon.ico" />
