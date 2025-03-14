0fb287d321d8517f1638f4e00a36cf23f3425c2b
Dale Larroder <dlarroder@gmail.com>
2025-03-14 18:23:07 +0800

    update

--- app/about/occupation.tsx
+++ app/about/occupation.tsx
@@ -5,7 +5,7 @@ export default function Occupation() {
     <div className="mb-8 flex flex-col-reverse items-center justify-between sm:flex-row sm:items-center">
       <div className="text-left">
         <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">Dale Larroder</h1>
-        <h2 className="text-sm font-normal md:text-base">Software Developer</h2>
+        <h2 className="text-sm font-normal md:text-base">Senior Full-Stack Developer</h2>
       </div>
       <div>
         <Image
