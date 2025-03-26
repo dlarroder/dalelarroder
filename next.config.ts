2300709983ab407e411f2efef704892d41555be8
Dale Larroder <dlarroder@gmail.com>
2025-03-26 19:12:05 +0800

    feat: umami analytics

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
+const umami_url = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_URL ?? '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx'],
   transpilePackages: ['next-mdx-remote'],
+  async rewrites() {
+    return [
+      {
+        source: '/umami.js',
+        destination: `${umami_url}/script.js`,
+      },
+      {
+        source: '/api/send',
+        destination: `${umami_url}/api/send`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
