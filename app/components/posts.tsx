633155c641049603f824e977b2b4c215379109d4
Dale Larroder <dlarroder@gmail.com>
2025-03-27 23:23:29 +0800

    blog qol

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -4,15 +4,15 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { BlogPost } from '../blog/utils';
 
-export function BlogPosts({ posts }: { posts: BlogPost[] }) {
+export function Posts({ posts }: { posts: BlogPost[] }) {
   return (
-    <ul>
+    <ul className="mt-6">
       {posts.map((post, index) => (
         <motion.li
           key={post.slug}
-          className="py-2"
-          initial={{ scale: 0.8, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
+          className="py-2 border-b border-gray-200 dark:border-gray-800"
+          initial={{ scale: 0.8, opacity: 0, filter: 'blur(2px)' }}
+          animate={{ scale: 1, opacity: 1, filter: 'blur(0px)' }}
           transition={{ duration: 0.6, delay: index / 10 }}
         >
           <Link
@@ -23,14 +23,10 @@ export function BlogPosts({ posts }: { posts: BlogPost[] }) {
             <article className="cursor-pointer gap-3 space-y-2 bg-opacity-20 py-5 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
               <div className="space-y-3 xl:col-span-4">
                 <span className="text-2xl font-bold leading-8 tracking-tight">
-                  <Link href={`/blog/${post.slug}`}>
-                    <span className="text-primary-500 duration-300 hover:text-primary-400">
-                      {post.metadata.title}
-                    </span>
-                  </Link>
+                  <Link href={`/blog/${post.slug}`}>{post.metadata.title}</Link>
                 </span>
 
-                <div className="max-w-none text-gray-900 dark:text-gray-100">
+                <div className="max-w-none text-gray-500 dark:text-gray-400">
                   {post.metadata.summary}
                 </div>
               </div>
