99c0c8413f4bfd5c9c03e5529ab5a6a1d6706470
Dale Larroder <dlarroder@gmail.com>
2025-03-27 22:54:26 +0800

    fragment

--- app/components/analytics/analytics.tsx
+++ app/components/analytics/analytics.tsx
@@ -4,16 +4,17 @@ import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import LogRocket from 'app/components/analytics/log-rocket';
 import Script from 'next/script';
+import { Fragment } from 'react';
 
 const umamiWebsiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID ?? '';
 
 export default function Analytics() {
   return (
-    <>
+    <Fragment>
       <Script defer src="/umami.js" data-website-id={umamiWebsiteId} />
       <LogRocket />
       <VercelAnalytics />
       <SpeedInsights />
-    </>
+    </Fragment>
   );
 }
