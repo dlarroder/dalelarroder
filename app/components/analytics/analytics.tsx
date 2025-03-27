e32f80b142d3d6e1d69e2fe98eedad7e259493ef
Dale Larroder <dlarroder@gmail.com>
2025-03-27 22:31:34 +0800

    chore: move umami to analytics

--- app/components/analytics/analytics.tsx
+++ app/components/analytics/analytics.tsx
@@ -3,10 +3,14 @@
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import LogRocket from 'app/components/analytics/log-rocket';
+import Script from 'next/script';
+
+const umamiWebsiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID ?? '';
 
 export default function Analytics() {
   return (
     <>
+      <Script defer src="/umami.js" data-website-id={umamiWebsiteId} />
       <LogRocket />
       <VercelAnalytics />
       <SpeedInsights />
