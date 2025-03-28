04d2eab437a49ed07d69c85e073d27d65b15072c
Dale Larroder <dlarroder@gmail.com>
2025-03-28 19:45:51 +0800

    posts

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -30,40 +30,38 @@ export default function Hero() {
       </h1>
       <div className="relative z-10 flex h-[calc(100vh-81px)] items-center md:h-[calc(100vh-116px)]">
         <div className="mx-auto w-screen max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0">
-          <div className="-mt-36">
-            <div ref={ref} className="flex cursor-default flex-col space-y-2">
-              <h1 className="text-5xl font-semibold sm:text-7xl md:text-8xl xl:text-9xl">
-                Dale Larroder
-              </h1>
-              <h2 className="text-3xl font-medium opacity-80 sm:text-6xl md:text-6xl xl:text-7xl">
-                I build things for the web.
-              </h2>
-              <Link
-                href="/about"
-                className="underline-magical text-md w-max cursor-pointer sm:text-lg md:text-xl xl:text-2xl"
-              >
-                Read more about me &rarr;
-              </Link>
-            </div>
-            <motion.div
-              animate={{
-                transform: `translateY(${progress * 10}vh)`,
-              }}
-              className="absolute bottom-4 left-1/2 -translate-x-1/2 transform md:bottom-8"
+          <div ref={ref} className="flex cursor-default flex-col space-y-2 -mt-36">
+            <h1 className="text-5xl font-semibold sm:text-7xl md:text-8xl xl:text-9xl">
+              Dale Larroder
+            </h1>
+            <h2 className="text-3xl font-medium opacity-80 sm:text-6xl md:text-6xl xl:text-7xl">
+              I build things for the web.
+            </h2>
+            <Link
+              href="/about"
+              className="underline-magical text-md w-max cursor-pointer sm:text-lg md:text-xl xl:text-2xl"
             >
-              <div
-                role="presentation"
-                className="flex cursor-pointer flex-col items-center justify-center"
-                onClick={() => {
-                  const intro = document.querySelector('#intro');
-
-                  intro?.scrollIntoView({ behavior: 'smooth' });
-                }}
-              >
-                <HiOutlineArrowNarrowDown size={20} />
-              </div>
-            </motion.div>
+              Read more about me &rarr;
+            </Link>
           </div>
+          <motion.div
+            animate={{
+              transform: `translateY(${progress * 10}vh)`,
+            }}
+            className="absolute bottom-4 left-1/2 -translate-x-1/2 transform md:bottom-8"
+          >
+            <div
+              role="presentation"
+              className="flex cursor-pointer flex-col items-center justify-center"
+              onClick={() => {
+                const intro = document.querySelector('#intro');
+
+                intro?.scrollIntoView({ behavior: 'smooth' });
+              }}
+            >
+              <HiOutlineArrowNarrowDown size={20} />
+            </div>
+          </motion.div>
         </div>
       </div>
       <canvas className="bg-skin-base pointer-events-none absolute inset-0" id="canvas"></canvas>
--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -10,25 +10,18 @@ export function Posts({ posts }: { posts: BlogPost[] }) {
       {posts.map((post, index) => (
         <motion.li
           key={post.slug}
-          className="py-2 border-b border-gray-200 dark:border-gray-800"
+          className="border-b border-gray-200 dark:border-gray-800"
           initial={{ scale: 0.8, opacity: 0, filter: 'blur(2px)' }}
           animate={{ scale: 1, opacity: 1, filter: 'blur(0px)' }}
           transition={{ duration: 0.6, delay: index / 10 }}
         >
-          <Link
-            href={`/blog/${post.slug}`}
-            aria-label={`Read "${post.metadata.title}"`}
-            legacyBehavior
-          >
-            <article className="cursor-pointer gap-3 space-y-2 bg-opacity-20 py-5 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-              <div className="space-y-3 xl:col-span-4">
-                <span className="text-2xl font-bold leading-8 tracking-tight">
-                  <Link href={`/blog/${post.slug}`}>{post.metadata.title}</Link>
-                </span>
-
-                <div className="max-w-none text-gray-500 dark:text-gray-400">
-                  {post.metadata.summary}
-                </div>
+          <Link href={`/blog/${post.slug}`} aria-label={`Read "${post.metadata.title}"`}>
+            <article className="py-5 flex flex-col space-y-3">
+              <span className="text-2xl font-bold leading-8 tracking-tight">
+                {post.metadata.title}
+              </span>
+              <div className="max-w-none text-gray-500 dark:text-gray-400">
+                {post.metadata.summary}
               </div>
             </article>
           </Link>
