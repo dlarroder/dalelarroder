ee426174f590fa82ab5e8291df1160fd993ea060
Dale Larroder <dlarroder@gmail.com>
2025-03-29 22:00:23 +0800

    chore: use next/fonts instead of fontsource

---
+++ app/fonts.ts
@@ -0,0 +1,9 @@
+import { Mukta } from 'next/font/google';
+
+export const mukta = Mukta({
+  weight: ['200', '300', '400', '500', '600', '700'],
+  variable: '--font-mukta',
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+});
