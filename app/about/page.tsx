55a0ebc6ad4ec8c668c9345ca6671ba163f38f8b
Donovan Riaño <donovanriano@gmail.com>
2025-04-11 13:39:24 -0600

    Update page.tsx rebranding xoco

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,8 @@ import MainLayout from '@/layouts/MainLayout';
 import { allAuthors } from 'contentlayer/generated';
 
 export const metadata = {
-  title: 'About - Anónimo Café',
-  description: 'About me - Dale Larroder',
+  title: 'About - Xoco Café',
+  description: 'About team - Xoco Café',
 };
 
 export default function About() {
