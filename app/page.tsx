6537b56fc2ba5dbc3c2aaea114b7e848d8bf1b07
Dale Larroder <dlarroder@gmail.com>
2025-04-18 11:18:15 +0800

    home copy fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,29 @@ import { AtSignIcon } from './components/layouts/icons/at-sign-icon';
 import { GithubIcon } from './components/layouts/icons/github-icon';
 import { LinkedinIcon } from './components/layouts/icons/linkedin-icon';
 import { XIcon } from './components/layouts/icons/x-icon';
-import SecondsSinceDate from './components/seconds-since-date';
-import { merryWeather, mukta } from './fonts';
+import { merryWeather } from './fonts';
 
 export default function Home() {
   return (
     <div className="min-h-svh w-screen overflow-hidden">
       <GooeyBackground />
-      <main className="relative z-10 flex min-h-svh max-w-5xl flex-col justify-center gap-4 px-8 md:px-24 text-shadow-lg lg:ml-14">
-        <h1
-          className={classNames(
-            'max-w-6xl font-serif text-2xl font-medium md:mr-4 md:text-4xl',
-            merryWeather.className
-          )}
-        >
+      <main
+        className={classNames(
+          'relative z-10 flex min-h-svh max-w-5xl flex-col justify-center gap-4 px-8 md:px-24 text-shadow-lg lg:ml-14',
+          merryWeather.className
+        )}
+      >
+        <h1 className="max-w-6xl font-serif text-2xl font-medium md:mr-4 md:text-4xl">
           Welcome to my <span className="font-bold">personal portfolio — </span> or, as I like to
           call it, my <span className="italic border-b">playground</span> on the web.
         </h1>
         <section>
-          <p
-            className={classNames('text-base md:text-lg text-justify font-light', mukta.className)}
-          >
-            <span className="mr-2 text-xs opacity-50">{'<p>'}</span>
-            I&apos;m Dale Larroder, a <SecondsSinceDate date={new Date('July 22, 1997')} /> second
-            old Software Engineer — and forever a student of the craft. I love building things for
-            the web and am always on the lookout for new challenges and opportunities to learn.
-            I&apos;m passionate about creating beautiful and functional user experiences. I believe
-            the best way to learn is by doing, so I&apos;m always exploring new projects. Currently,
-            I&apos;m working as a Software Engineer at a{' '}
+          <p className="text-base md:text-lg text-justify font-light">
+            I&apos;m Dale Larroder — a Software Engineer and forever a student of the craft. I love
+            building things for the web and am always on the lookout for new challenges and
+            opportunities to learn. I&apos;m passionate about creating beautiful and functional user
+            experiences. I believe the best way to learn is by doing, so I&apos;m always exploring
+            new projects. Currently, I&apos;m working as a Software Engineer at a{' '}
             <a
               href="https://www.aphex.co/"
               className="underline-magical"
@@ -41,13 +36,12 @@ export default function Home() {
             >
               Aphex
             </a>
-            .<span className="ml-2 text-xs opacity-50">{'<p>'}</span>
           </p>
         </section>
         <section className="flex space-x-4 items-center text-sm md:text-base">
           <div>
             <p>More about me: </p>
-            <div className="flex">
+            <div className="flex -ml-2">
               <Link
                 href="https://www.linkedin.com/in/dale-larroder/"
                 target="_blank"
@@ -78,7 +72,7 @@ export default function Home() {
             </div>
           </div>
           <div className="h-14 border-l border-gray-300" />
-          <div className={classNames('flex flex-wrap space-x-3 space-y-1', merryWeather.className)}>
+          <div className="flex flex-wrap space-x-3 space-y-1">
             <Link href="/projects">/projects</Link>
             <Link href="/thoughts">/thoughts</Link>
             <Link href="/uses">/uses</Link>
