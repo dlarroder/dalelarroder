8c9df8e67732ef5418a0a3e11de72c856a9499af
Dale Larroder <dlarroder@gmail.com>
2025-04-18 11:17:57 +0800

    small layout tweaks

--- app/components/layouts/page-container.tsx
+++ app/components/layouts/page-container.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PageContainer({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <main className="flex min-h-svh w-full max-w-5xl xl:max-w-6xl flex-col gap-4 border-gray-200 dark:border-gray-300/20 p-8 pt-16 md:p-24 border-x">
+      <main className="flex min-h-svh w-full max-w-5xl flex-col gap-4 border-gray-200 dark:border-gray-300/20 p-8 pt-16 md:p-18 border-x">
         {children}
       </main>
     </div>
--- app/uses/uses-title.tsx
+++ app/uses/uses-title.tsx
@@ -1,7 +1,7 @@
 export default function UsesTitle() {
   return (
     <div className="mb-2 space-y-2">
-      <h1 className="text-4xl">Here are the tools I use to build things.</h1>
+      <h1 className="text-2xl md:text-4xl">Here are the tools I use to build things.</h1>
       <span className="text-gray-700  dark:text-gray-300">
         Inspired by{' '}
         <a
