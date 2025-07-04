2d4fa628a42f38d4a36128a6d38353e27249d698
Dale Larroder <dlarroder@gmail.com>
2025-07-05 03:07:07 +0800

    update

---
+++ app/components/hero-v2/arrow-down.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { ArrowDownIcon } from '../layouts/icons/arrow-down-icon';
+
+export default function ArrowDown() {
+  return (
+    <button
+      onClick={() => {
+        const intro = document.querySelector('#intro');
+
+        intro?.scrollIntoView({ behavior: 'smooth' });
+      }}
+      className="z-10 absolute bottom-5 left-1/2 transform -translate-x-1/2 cursor-pointer dark:text-white"
+    >
+      <ArrowDownIcon size={18} />
+    </button>
+  );
+}
