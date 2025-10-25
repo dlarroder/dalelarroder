ebf19715523417f2a961633c97f22e4869ec77a5
Dale Larroder <dlarroder@gmail.com>
2025-10-25 19:37:36 +0800

    fix: build

--- app/thoughts/utils.ts
+++ app/thoughts/utils.ts
@@ -23,10 +23,10 @@ function parseFrontmatter(fileContent: string) {
 	const match = frontmatterRegex.exec(fileContent);
 	const frontMatterBlock = match?.[1];
 	const content = fileContent.replace(frontmatterRegex, '').trim();
-	const frontMatterLines = frontMatterBlock.trim().split('\n');
+	const frontMatterLines = frontMatterBlock?.trim().split('\n');
 	const metadata: Partial<Metadata> = {};
 
-	frontMatterLines.forEach((line) => {
+	frontMatterLines?.forEach((line) => {
 		const [key, ...valueArr] = line.split(': ');
 		let value = valueArr.join(': ').trim();
 		value = value.replace(/^['"](.*)['"]$/, '$1'); // Remove quotes
