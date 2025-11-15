c5b52865bd32fc4162ef99756737e1562d885c20
Dale Larroder <dlarroder@gmail.com>
2025-11-15 13:00:02 +0800

    chore: remove toc

--- app/thoughts/[slug]/page.tsx
+++ app/thoughts/[slug]/page.tsx
@@ -1,10 +1,7 @@
-import path from 'node:path';
 import type { Metadata } from 'next';
 import BackNavigation from '../../components/layouts/back-navigation';
-import { formatDate, getPostFromSlug, readMDXFile } from '../utils';
-import { extractHeadings } from './extract-headings';
+import { formatDate, getPostFromSlug } from '../utils';
 import PageTitle from './page-title';
-import TableOfContents from './table-of-contents';
 
 export async function generateMetadata(props: {
 	params: Promise<{ slug: string }>;
@@ -53,12 +50,6 @@ export default async function Blog(props: {
 
 	const { metadata, content } = await getPostFromSlug(params.slug);
 
-	// Extract headings from the raw MDX content for TOC
-	const rawContent = readMDXFile(
-		path.join(process.cwd(), 'app/thoughts/posts', `${params.slug}.mdx`),
-	);
-	const headings = extractHeadings(rawContent.content);
-
 	return (
 		<>
 			<section>
@@ -70,10 +61,7 @@ export default async function Blog(props: {
 					</p>
 				</div>
 			</section>
-			<div className='relative lg:grid lg:grid-cols-[1fr_250px] lg:gap-12'>
-				<article className='md:max-w-5xl'>{content}</article>
-				<TableOfContents headings={headings} />
-			</div>
+			<article className='md:max-w-5xl'>{content}</article>
 		</>
 	);
 }
